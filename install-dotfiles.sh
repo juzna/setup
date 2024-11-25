a8184d5f828439179740b7bf32c185379a845597
Jan Dolecek <jan.dolecek@kiwi.com>
2024-11-25 12:38:34 +0100

    support instaling secret dotfiles

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,25 +1,23 @@
 #!/bin/zsh
-#
 # Install all files from dotfiles/, by linking them to $HOME dir.
-#
 
 cd $(dirname $0)
 
-for F in dotfiles/.*; do
-  F=$(basename $F)
-  FULL=$PWD/dotfiles/$F
+for F in dotfiles/.* secrets/dotfiles/.*; do
+  BASE=$(basename $F)
+  FULL=$PWD/$F
 
-  if [[ -h "$HOME/$F" ]]; then  # it's a symlink
-    L=$(readlink "$HOME/$F")
+  if [[ -h "$HOME/$BASE" ]]; then  # it's a symlink
+    L=$(readlink "$HOME/$BASE")
     if [[ "$L" != "$FULL" ]]; then
-      >&2 echo "$HOME/$F already exists and link to a wrong file ($L != $FULL), remove it first"
+      >&2 echo "$HOME/$BASE already exists and link to a wrong file ($L != $FULL), remove it first"
     else
       #>&2 echo "$HOME/$F is already linked"
     fi
-  elif [[ -f "$HOME/$F" ]]; then  # it's a refular file
-    >&2 echo "$HOME/$F already exists, remove it first"
+  elif [[ -f "$HOME/$BASE" ]]; then  # it's a regular file
+    >&2 echo "$HOME/$BASE already exists, remove it first"
   else
-    echo "Linking $HOME/$F -> $FULL"
-    ln -s "$FULL" "$HOME/$F"
+    echo "Linking $HOME/$BASE -> $FULL"
+    ln -s "$FULL" "$HOME/$BASE"
   fi
 done
